23221e5c76d5ff10213dd04b7ad0a2ec86163fcf
Jose Rodriguez <joserodriguez@Joses-MacBook-Pro.local>
2017-12-08 14:01:46 -0800

    change some html

--- beep-boop/js/scripts.js
+++ beep-boop/js/scripts.js
@@ -1,17 +1,38 @@
-$(function(){
-  $('#wordForm').submit(function(event){
+//Business Logic
+
+var numberArray = function(input) {
+  var userList = [];
+  if (isNaN(input)) {
+    return "Input error: Please enter a numerical value.";
+  } else
+  {
+    for (var i = 0; i <= userInput; i++) {
+      if ((i % 3) === 0) {
+        return "I'm sorry, Dave. I'm afraid I can't do that!";
+      } else if {
+        for (var j = 0; j < arr[i].length; j++) {
+          if (j === 0) {
+            return i.splice(j, 1, "Beep!");
+          } else if (j === 1) {
+            return i.splice(j, 1, "Boop!");
+          })
+      })
+
+
+  }
+}
+}
+
+//User Interface Logic
+
+var userInput;
+var userOutput;
+$(document).ready(function() {
+  $("#number-form").submit(function(event) {
     event.preventDefault();
-    var word = $('#enterWords').val();
-    codingWord = [];
-    for(i =0; i<= word.length; i++){
-      if(word[i]==='a' || word[i]==='e' ||word[i]==='i' ||word[i]==='o' ||word[i]==='u'){
-        codingWord[i]='_';
-      }
-      else {
-        codingWord[i]= word[i];
-      }
-    }
-      var codingWordString = codingWord.slice();
-      $("#codingWord").text(codingWordString.join(''));
+    userInput = parseInt($("#user-number").val());
+
+    userOutput = numberarray(userInput);
+    alert(userOutput);
+
   });
-});
